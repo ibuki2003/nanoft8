6cb5ca49f8618237f8e4a1fdabf402536c1425b3
ibuki2003 <main@fuwa.dev>
2024-10-25 01:05:28 +0900

    fix(bits): 64bit slice

--- src/bits.rs
+++ src/bits.rs
@@ -33,16 +33,34 @@ impl Bitset {
         } else {
             let len1 = 32 - start_bit;
             let len2 = size - len1;
-            set_range(&mut self.0[start_word], start_bit, len1, value >> (size - len1));
-            set_range(&mut self.0[start_word + 1], 0, len2, value & ((1 << len2) - 1));
+            set_range(
+                &mut self.0[start_word],
+                start_bit,
+                len1,
+                value >> (size - len1),
+            );
+            set_range(
+                &mut self.0[start_word + 1],
+                0,
+                len2,
+                value & ((1 << len2) - 1),
+            );
         }
     }
 
     #[inline]
     pub fn slice_u64(&self, start: usize, size: usize) -> u64 {
-        let mut v = (self.slice(start, size.min(32)) as u64) << 32;
-        if size > 32 {
-            v |= self.slice(start + 32, size - 32) as u64;
+        debug_assert!(size <= 64);
+        debug_assert!(start + size <= Self::SIZE);
+        let start_word = start / 32;
+        let start_bit = start % 32;
+
+        let mut v = (self.0[start_word] as u64) << (32 + start_bit);
+        if start_bit + size > 32 {
+            v |= (self.0[start_word + 1] as u64) << start_bit;
+        }
+        if start_bit + size > 64 {
+            v |= (self.0[start_word + 2] as u64) >> (32 - start_bit);
         }
         v >> (64 - size)
     }
